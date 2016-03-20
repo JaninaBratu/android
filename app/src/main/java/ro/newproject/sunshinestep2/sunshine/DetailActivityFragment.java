3f2f123af2a0a678a7828898da64d83fab9a8ea1
Bratu Janina Ninetta <Bratu Janina Ninetta>
2016-03-20 21:06:23 +0200

    Detail Activity with Intent and ClickListener

---
+++ app/src/main/java/ro/newproject/sunshinestep2/sunshine/DetailActivityFragment.java
@@ -0,0 +1,40 @@
+package ro.newproject.sunshinestep2.sunshine;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DetailActivityFragment extends Fragment {
+
+    TextView textView;
+
+    public DetailActivityFragment() {
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        Intent intent = getActivity().getIntent();
+        View rootView = inflater.inflate(R.layout.fragment_detail, container);
+        if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)){
+            String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            ((TextView) rootView.findViewById(R.id.detailText)).
+                                                         setText(forecastStr);
+        }
+
+        return rootView;
+    }
+}
